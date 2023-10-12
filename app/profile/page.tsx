a0ba8213d88743bc05b0bdb67259a3ff37157f39
Samuel Oseh <samieoseh@gmail.com>
2023-10-12 05:33:36 +0100

    removed HankoProfile

--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -1,11 +1,11 @@
 import Container from "@/components/Container";
-import HankoProfile from "@/components/HankoProfile";
+// import HankoProfile from "@/components/HankoProfile";
 
 export default function ProfilePage() {
   return (
     <Container>
       <h1>Profile Page</h1>
-      <HankoProfile />
+      {/* <HankoProfile /> */}
     </Container>
   );
 }
