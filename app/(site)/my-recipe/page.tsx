866e34e14b57b5b277b6dfc6bf8cfd46c6f129f7
samieoseh <139955427+samieoseh@users.noreply.github.com>
2023-10-25 11:04:10 +0100

    Update page.tsx

--- app/(site)/my-recipe/page.tsx
+++ app/(site)/my-recipe/page.tsx
@@ -3,8 +3,7 @@ import { useQuery } from "@tanstack/react-query";
 
 const MyRecipePage = () => {
   // get all favorite recipes here
-  const { data } = useQuery(["favorites"], async () => await getFavorites());
-
+  
   return <Container>MyRecipePage</Container>;
 };
 
