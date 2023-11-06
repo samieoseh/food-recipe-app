9144e8c1cec5b5ced4811337c8e3929555917d4e
Samuel Oseh <samieoseh@gmail.com>
2023-11-06 08:30:21 +0100

    reverted back next.config

--- next.config.js
+++ next.config.js
@@ -1,16 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "spoonacular.com",
-      },
-      {
-        protocol: "https",
-        hostname: "images.spoonacular.com",
-      },
-    ],
+    domains: ["spoonacular.com", "images.spoonacular.com"],
   },
 };
 
