9e73e07c035e185dcc0efc516142c850c553be52
Samuel Oseh <samieoseh@gmail.com>
2023-11-06 09:19:32 +0100

    updated getUrl

--- lib/utils.ts
+++ lib/utils.ts
@@ -54,9 +54,7 @@ export const getFavoritesFromDB = async () => {
 };
 
 export const getUrl = () => {
-  const url =
-    process?.env?.NEXT_PUBLIC_SITE_URL ??
-    process?.env?.VERCEL_URL ??
-    "http://localhost:3000/";
+  console.log(process?.env?.NEXT_PUBLIC_VERCEL_URL);
+  const url = process?.env?.NEXT_PUBLIC_VERCEL_URL ?? "http://localhost:3000/";
   return url + "/dashboard";
 };
