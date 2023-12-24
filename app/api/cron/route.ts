0ddf5a65e4352210717abeda32405bfcb3ffa9b3
Samuel Oseh <samieoseh@gmail.com>
2023-12-24 08:37:15 +0100

    editted route.ts

--- app/api/cron/route.ts
+++ app/api/cron/route.ts
@@ -5,5 +5,14 @@ export async function GET(request: NextRequest) {
   // get all user in database
   const mealPlan = getMealPlanIfExist();
   console.log(mealPlan);
+  const result = await fetch(
+    "http://worldtimeapi.org/api/timezone/America/Chicago",
+    {
+      cache: "no-store",
+    }
+  );
+  const data = await result.json();
+  return Response.json({ datetime: data.datetime });
+
   //const recommendations = generateWeeklyMealPlan();
 }
